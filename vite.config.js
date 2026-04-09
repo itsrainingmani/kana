4a550de25ae23501bdf13ab0f6a22cdb8ac7d988
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2026-04-09 00:31:49 -0400

    dont use strictPort option for vite

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,12 @@ import { defineConfig, searchForWorkspaceRoot } from 'vite';
 export default defineConfig({
   server: {
     port: 4173,
-    strictPort: true,
     fs: {
       allow: [searchForWorkspaceRoot(process.cwd())]
     }
   },
   preview: {
     port: 4173,
-    strictPort: true
   },
   worker: {
     format: 'es'
